f3da1b8ddabdd1ed13ecd2d804429129c58e450f
NekaB <nkbrown57@gmail.com>
2020-04-14 13:31:11 -0400

    Updated:MachineSec

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "Employee_Security_Group" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["73.106.72.159/32"]
   }
 
 ingress {
@@ -19,7 +19,7 @@ ingress {
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["73.106.72.159/32"]
 }
 
 ingress {
@@ -27,10 +27,26 @@ description = "HTTPS for updates"
 from_port = 443
 to_port = 443
 protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+cidr_blocks = ["73.106.72.159/32"]
 }
 
 
+ingress {
+description = "Private Network"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["172.31.0.0/16"]
+}
+
+
+egress {
+description = "Universal Egress"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_security_group" "Customer_Security_Group" {
@@ -39,8 +55,8 @@ resource "aws_security_group" "Customer_Security_Group" {
 
   ingress {
     description = "SSH from my apartment"
-    from_port = 22
-    to_port = 22
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -49,28 +65,38 @@ resource "aws_security_group" "Customer_Security_Group" {
 }
 
 
-resource "aws_instance" "dev_machine" {
-  ami = "ami-08fe38a2865705db8"
-  instance_type = "t2.micro"
-
-  tags = { 
-    Name = "Dev"
- }
-
-}
 
 resource "aws_instance" "jump_machine" {
   ami = "ami-08fe38a2865705db8"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Employee_Security_Group.id]
+  key_name = "Nbrown"
+
+  tags = { 
+    Name = "Jump"
+  }
 }
 
+resource "aws_instance" "dev_machine" {
+  ami = "ami-08fe38a2865705db8"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Employee_Security_Group.id]
+  key_name = "Nbrown"
+
+  tags = { 
+    Name = "Dev"
+  }
+
+}
 resource "aws_instance" "prod_machine" {
   ami = "ami-08fe38a2865705db8"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Employee_Security_Group.id]
+  key_name = "Nbrown"
 
   tags = { 
     Name = "Prod"
- }
+  }
 
 }
 resource "aws_instance" "jenkins_machine" {
@@ -79,27 +105,12 @@ resource "aws_instance" "jenkins_machine" {
   vpc_security_group_ids = [aws_security_group.Employee_Security_Group.id]
   key_name = "Nbrown"
   
-  lifecycle {
-    		prevent_destroy = true
       
-  }
 
   tags = {
     Name = "Jenkins"
- } 
-
+  } 
 }
 
-resource "aws_instance" "web" {
-  ami = "ami-08fe38a2865705db8"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Employee_Security_Group.id]
-  key_name = "Nbrown"
-
-  tags = {
-    Name = "Web"
-  }
-
-} 
 
 
