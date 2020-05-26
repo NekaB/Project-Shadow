aa36ec334c25b54ae0e0d76e96b34928e8f4a580
NekaB <nkbrown57@gmail.com>
2020-05-26 14:07:52 -0400

    updatedsecjenk

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,23 @@ resource "aws_security_group" "Jump_Security" {
     protocol = "tcp"
     cidr_blocks = ["172.31.0.0/16"]
   }
+
+   ingress {
+    description = "SSH from my apartment"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port = 0
     to_port = 0
     protocol ="-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 
 
   tags = {
